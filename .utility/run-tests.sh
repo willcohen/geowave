d0681eb17db25c11c5333be724021e8704398944
Rich Fecher <rfecher@gmail.com>
2018-02-13 07:57:38 -0500

    wip

--- .utility/run-tests.sh
+++ .utility/run-tests.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "$IT_ONLY" == "true" ]; then
   echo -e "Skipping unit tests w/ verify...\n"
-  mvn -q verify -am -pl test -Dtest=SkipUnitTests -DfailIfNoTests=false -P $MAVEN_PROFILES
+  mvn verify -am -pl test -Dtest=SkipUnitTests -DfailIfNoTests=false -P $MAVEN_PROFILES
 else
   echo -e "Running unit tests only w/ verify...\n"
   mvn -q verify -am -pl test -P $MAVEN_PROFILES
